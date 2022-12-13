7025f8d197a64de271941e39f1adc381ba1922c9
TriThai <trithai45@gmail.com>
2022-12-13 14:32:40 -0600

    comments

--- test_action/main.py
+++ test_action/main.py
@@ -2,10 +2,19 @@ import pandas as pd
 
 
 def get_empty_df():
+    """
+    Gets an empty dataframe
+    :return: an empty dataframe
+    """
     return pd.DataFrame([])
 
 
 def build_msg(val):
+    """
+    Build a message
+    :param val: a value
+    :return: a message
+    """
     return f'value: {val}'
 
 
